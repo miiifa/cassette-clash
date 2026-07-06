ff3c4c8e795526036f17e0c633b119a3f1a913d3
miiifa <86800867+miiifa@users.noreply.github.com>
2026-07-06 14:41:40 +0900

    Add Android WebView activity

---
+++ app/src/main/java/com/miiifa/pokemondueloffline/MainActivity.java
@@ -0,0 +1,40 @@
+package com.miiifa.pokemondueloffline;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        webView.setWebViewClient(new WebViewClient());
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        settings.setAllowContentAccess(true);
+
+        webView.setOverScrollMode(View.OVER_SCROLL_NEVER);
+        webView.loadUrl("file:///android_asset/play.html");
+
+        setContentView(webView);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView != null && webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
