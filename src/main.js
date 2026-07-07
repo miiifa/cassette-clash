48f250a95857960ef31ae76790abc9fafabac0dd
miiifa <86800867+miiifa@users.noreply.github.com>
2026-07-07 11:26:01 +0900

    Add split main module

---
+++ src/main.js
@@ -0,0 +1,5 @@
+window.KOMA=window.KOMA||{};
+(function(K){
+K.start=function(){K.initState();K.render();};
+window.addEventListener('DOMContentLoaded',()=>{K.bindUi();K.start();});
+})(window.KOMA);
